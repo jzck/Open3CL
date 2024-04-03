cde4dbcd3a44eefaa041b2648e501804e570c19d
Florent Perinel <florent.perinel@redfroggy.fr>
2024-04-03 09:46:52 +0200

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+/** @type {import('jest').Config} */
+const config = {
+  // Automatically clear mock calls, instances, contexts and results before every test
+  clearMocks: true,
+  // Indicates whether the coverage information should be collected while executing the test
+  collectCoverage: true,
+  // The directory where Jest should output its coverage files
+  coverageDirectory: 'coverage',
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: 'v8'
+};
+
+export default config;
