01a1dd5ba37a070a560ba2641fc7cc5ebdc6cfdf
Michael Desigaud <michael.desigaud@redfroggy.fr>
2024-04-05 09:19:12 +0200

    feat: synchronize ademe xlsx and ods files

---
+++ src/core/assets/domain/synchronize-enum-tables.js
@@ -0,0 +1,90 @@
+import { diff } from 'deep-object-diff';
+import enums from '../../../enums.js';
+
+/**
+ * Download the `enum_tables.xlsx` file from the official ademe repository and generates a new `enums.js` file
+ * Please see @link https://gitlab.com/observatoire-dpe/observatoire-dpe/-/tree/master
+ * - Convert the file into a json object
+ * - Extract, format data and then generate a new enums.js file
+ */
+export class SynchronizeEnumTables {
+  /**
+   * @type {FileStore}
+   */
+  #fileStore;
+
+  /**
+   * @type {ApplicationConfig}
+   */
+  #appConfig;
+
+  /**
+   * @param fileStore {FileStore}
+   * @param appConfig {ApplicationConfig}
+   */
+  constructor(fileStore, appConfig) {
+    this.#fileStore = fileStore;
+    this.#appConfig = appConfig;
+  }
+
+  /**
+   * @return {Promise<void>}
+   */
+  async execute() {
+    return await this.#fileStore
+      .downloadXlsxFileAndConvertToJson(this.#appConfig.ademeEnumTablesFileUrl)
+      .then(
+        /** @param excelSheets {{[tabName: string]: {id: string, lib: string}[]}} xlsx content file grouped by tab */
+        (excelSheets) => {
+          const enumsOutput = {};
+
+          // for each tab in xlsx file
+          Object.keys(excelSheets)
+            .filter((sheetName) => sheetName !== 'index') // Exclude first tab called "index"
+            .forEach((sheetName) => {
+              /**
+               * Each Excel sheet has a list of columns
+               * Keep only the two first columns for now for compatibility with the legacy `enums.js` file
+               * @type {{id: string, lib: string}[]}
+               */
+              const excelSheetValues = excelSheets[sheetName];
+
+              const outputTabValues = {};
+
+              /**
+               * Convert object in the form of [{id: 1, lib: "test"}, {id: 2, lib: "test 2"}]
+               * to {1: "test", 2: "test 2"}
+               */
+              excelSheetValues.forEach((excelSheetValue) => {
+                outputTabValues[excelSheetValue.id] = excelSheetValue.lib.replace(/ :/g, ' :');
+
+                // For compatibility with the legacy `enums.js` file, force the lowercase for
+                // each value except the `classe_etiquette`
+                if (sheetName !== 'classe_etiquette') {
+                  outputTabValues[excelSheetValue.id] =
+                    outputTabValues[excelSheetValue.id].toLowerCase();
+                }
+              });
+
+              enumsOutput[sheetName] = outputTabValues;
+            });
+
+          // Verify that the generated file has no diff with the legacy `enum.js` file
+          const enumsDiff = diff(enums, enumsOutput);
+
+          if (Object.keys(enumsDiff).length > 0) {
+            console.error(enumsDiff);
+            return Promise.reject(
+              'Enums file from ademe repository is different from legacy file: enum.js'
+            );
+          }
+
+          // Overwrite the enums.js file in filesystem
+          return this.#fileStore.writeFileToLocalSystem(
+            `${this.#appConfig.assetsOutputFolder}/enums.js`,
+            `/** @type {TableEnum} **/\nexport default ${JSON.stringify(enumsOutput, null, 2)}`
+          );
+        }
+      );
+  }
+}
---
+++ src/core/assets/domain/synchronize-solicitations-tables.js
@@ -0,0 +1,68 @@
+import { set } from 'lodash-es';
+
+const ILPA_PROPERTY = 'ilpa';
+const CLASSE_ALTITUDE_PROPERTY = 'classe_altitude';
+const MOIS_PROPERTY = 'mois';
+const EXCLUDED_PROPERTIES = [ILPA_PROPERTY, CLASSE_ALTITUDE_PROPERTY, MOIS_PROPERTY];
+
+/**
+ * Read the `18.2_sollicitations_ext.ods` local file
+ * - Convert the file into a json object
+ * - Extract, format data and return data
+ *
+ *  The file content is merged in the `tv.js` file generated in the {@link SynchronizeValeurTables} use case.
+ */
+export class SynchronizeSolicitationsTables {
+  /**
+   * @type {FileStore}
+   */
+  #fileStore;
+
+  /**
+   * @type {ApplicationConfig}
+   */
+  #appConfig;
+
+  /**
+   * @param fileStore {FileStore}
+   * @param appConfig {ApplicationConfig}
+   */
+  constructor(fileStore, appConfig) {
+    this.#fileStore = fileStore;
+    this.#appConfig = appConfig;
+  }
+
+  /**
+   * @return {Promise<any>}
+   */
+  async execute() {
+    return this.#fileStore
+      .readLocalOdsFileAndConvertToJson(this.#appConfig.solicitationsExtFilePath)
+      .then(
+        /** @param excelSheets {{[key: string]: {ilpa?: number, classe_altitude: string, mois: string}[]}} **/ (
+          excelSheets
+        ) => {
+          const output = {};
+
+          // For each tab in Excel file
+          for (const sheetName in excelSheets) {
+            output[sheetName] = {};
+
+            excelSheets[sheetName].forEach((sheetValue) => {
+              // Group by "ilpa" property if it exists or "classe_altitude" otherwise
+              let groupKey = !!sheetValue.ilpa ? sheetValue.ilpa : sheetValue.classe_altitude;
+
+              for (const sheetValueKey in sheetValue) {
+                if (!EXCLUDED_PROPERTIES.includes(sheetValueKey)) {
+                  const path = `${sheetName}.${groupKey}${!!sheetValue.ilpa ? '.' + sheetValue.classe_altitude : ''}.${sheetValue.mois}.${sheetValueKey}`;
+                  set(output, path, parseFloat(sheetValue[sheetValueKey]));
+                }
+              }
+            });
+          }
+
+          return output;
+        }
+      );
+  }
+}
