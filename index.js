fe8fcf32f1fa9b5e5e21f76c8de1183f93b0b363
Jack Halford <jack@0x5.be>
2024-06-20 16:29:41 +0200

    fix: Export U functions for usage by lib users

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 import { calcul_3cl } from './src/index.js';
 export { calcul_3cl };
+import { Umur, Uph, Upb, Uporte, Ubv, Upt } from 'src/3_deperdition.js';
+export { Umur, Uph, Upb, Uporte, Ubv, Upt };
