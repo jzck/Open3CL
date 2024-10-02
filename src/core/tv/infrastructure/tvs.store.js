4b402fd02c15b954aff33b0b879bb1d16a3456cc
Jérôme GAVIGNET <jerome.gavignet@redfroggy.fr>
2024-10-02 15:59:33 +0200

    fix(feat-94): get rendement_distribution_ch from enum values

--- src/core/tv/infrastructure/tvs.store.js
+++ src/core/tv/infrastructure/tvs.store.js
@@ -2,27 +2,39 @@ import tvs from '../../../tv.js';
 
 const TV_RENDEMENT_DISTRIBUTION_CH_PROPERTY = 'rendement_distribution_ch';
 
+/**
+ * @type {RendementDistributionCh[]}
+ */
+const RENDEMENT_DISTRIBUTION_CH_LIST = tvs[TV_RENDEMENT_DISTRIBUTION_CH_PROPERTY];
+
 export class TvsStore {
   /**
    * @param enumTypeEmissionDistributionId {string}
-   * @param reseauDistributionIsole {string?}
+   * @param reseauDistributionIsole {number?}
    * @return {RendementDistributionCh}
    */
   getRendementDistributionCh(enumTypeEmissionDistributionId, reseauDistributionIsole) {
-    /**
-     *
-     * @type {RendementDistributionCh[]}
-     */
-    const rendement_distribution_ch_list = tvs[TV_RENDEMENT_DISTRIBUTION_CH_PROPERTY];
-    return rendement_distribution_ch_list.find((rendement) => {
+    return RENDEMENT_DISTRIBUTION_CH_LIST.find((rendement) => {
       const hasTypeEmissionId = rendement.enum_type_emission_distribution_id
         .split('|')
         .includes(enumTypeEmissionDistributionId);
-      if (hasTypeEmissionId && reseauDistributionIsole) {
-        return rendement.reseau_distribution_isole === reseauDistributionIsole;
+
+      if (hasTypeEmissionId && reseauDistributionIsole !== undefined) {
+        return rendement.reseau_distribution_isole === reseauDistributionIsole.toString();
       }
 
       return hasTypeEmissionId;
     });
+  }
+
+  /**
+   * @param tv_rendement_distribution_ch_id {number}
+   * @return {RendementDistributionCh}
+   */
+  getRendementDistributionChById(tv_rendement_distribution_ch_id) {
+    return RENDEMENT_DISTRIBUTION_CH_LIST.find(
+      (rendement) =>
+        rendement.tv_rendement_distribution_ch_id === tv_rendement_distribution_ch_id.toString()
+    );
   }
 }
