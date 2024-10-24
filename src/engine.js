77190a864b59e734fdebf596c2791438da7ae074
Jérôme GAVIGNET <jerome.gavignet@redfroggy.fr>
2024-10-24 18:02:59 +0200

    feat: Virtualization or proration of ECS needs

--- src/engine.js
+++ src/engine.js
@@ -55,9 +55,57 @@ export function calcul_3cl(dpe) {
   dpe.administratif.diagnostiqueur = { version_moteur_calcul: `Open3CL ${package_version}` };
   const env = logement.enveloppe;
   let Sh;
+  let ShEcs;
   // TODO requestInput Sh
   if (th === 'maison' || th === 'appartement') Sh = cg.surface_habitable_logement;
   else if (th === 'immeuble') Sh = cg.surface_habitable_immeuble;
+
+  /**
+   * Certains DPE appartement sont générés à partir des données du DPE immeuble, la surface à prendre en compte est
+   * celle de l'immeuble pour les besoins ECS
+   * 10 - dpe appartement généré à partir des données DPE immeuble chauffage individuel ecs individuel
+   * 11 - dpe appartement généré à partir des données DPE immeuble chauffage collectif ecs individuel
+   * 12 - dpe appartement généré à partir des données DPE immeuble chauffage individuel ecs collectif
+   * 13 - dpe appartement généré à partir des données DPE immeuble chauffage collectif ecs collectif
+   * 15 - dpe issu d'une étude thermique réglementaire RT2012 bâtiment : appartement chauffage collectif ecs collectif
+   * 16 - dpe issu d'une étude thermique réglementaire RT2012 bâtiment : appartement chauffage individuel ecs collectif
+   * 19 - dpe issu d'une étude energie environement réglementaire RE2020 bâtiment : appartement chauffage collectif ecs collectif
+   * 20 - dpe issu d'une étude energie environement réglementaire RE2020 bâtiment : appartement chauffage individuel ecs collectif
+   * 22 - dpe issu d'une étude thermique réglementaire RT2012 bâtiment : appartement chauffage individuel ecs individuel
+   * 23 - dpe issu d'une étude thermique réglementaire RT2012 bâtiment : appartement chauffage collectif ecs individuel
+   * 24 - dpe issu d'une étude energie environement réglementaire RE2020 bâtiment : appartement chauffage collectif ecs individuel
+   * 25 - dpe issu d'une étude energie environement réglementaire RE2020 bâtiment : appartement chauffage individuel ecs individuel
+   * 33 - dpe appartement généré à partir des données DPE immeuble chauffage mixte (collectif-individuel) ecs individuel
+   * 34 - dpe appartement généré à partir des données DPE immeuble chauffage mixte (collectif-individuel) ecs collectif
+   * 38 - dpe appartement généré à partir des données DPE immeuble chauffage mixte (collectif-individuel) ecs mixte (collectif-individuel)
+   * 39 - dpe appartement généré à partir des données DPE immeuble chauffage individuel ecs mixte (collectif-individuel)
+   * 40 - dpe appartement généré à partir des données DPE immeuble chauffage collectif ecs mixte (collectif-individuel)
+   */
+  if (
+    [
+      '10',
+      '11',
+      '12',
+      '13',
+      '15',
+      '16',
+      '19',
+      '20',
+      '22',
+      '23',
+      '24',
+      '25',
+      '33',
+      '34',
+      '38',
+      '39',
+      '40'
+    ].includes(map_id)
+  ) {
+    ShEcs = cg.surface_habitable_immeuble;
+  } else {
+    ShEcs = Sh;
+  }
 
   const zc_id = logement.meteo.enum_zone_climatique_id;
   const ca_id = logement.meteo.enum_classe_altitude_id;
@@ -92,7 +140,7 @@ export function calcul_3cl(dpe) {
     th,
     ecs,
     clim,
-    Sh,
+    ShEcs,
     Nb_lgt,
     GV,
     ilpa,
@@ -102,11 +150,22 @@ export function calcul_3cl(dpe) {
 
   const bfr = apport_et_besoin.besoin_fr;
   const bfr_dep = apport_et_besoin.besoin_fr_depensier;
-  clim.forEach((clim) => calc_clim(clim, bfr, bfr_dep, zc_id, Sh));
+  clim.forEach((clim) => calc_clim(clim, bfr, bfr_dep, zc_id, ShEcs));
 
-  const becs = apport_et_besoin.besoin_ecs;
+  /**
+   * La consommation ECS est obtenu pour certains types de DPE par virtualisation des générateurs collectifs en générateurs individuels virtuels
+   * 4 - dpe appartement individuel chauffage individuel ecs collectif
+   * 5 - dpe appartement individuel chauffage collectif ecs collectif
+   * 32 - dpe appartement individuel chauffage mixte (collectif-individuel) ecs collectif
+   * 35 - dpe appartement individuel chauffage mixte (collectif-individuel) ecs mixte (collectif-individuel)
+   * 36 - dpe appartement individuel chauffage individuel ecs mixte (collectif-individuel)
+   * 37 - dpe appartement individuel chauffage collectif ecs mixte (collectif-individuel)
+   */
+  const virtualisationECS = ['4', '5', '32', '35', '36', '37'].includes(map_id);
+
+  let becs = apport_et_besoin.besoin_ecs;
   const becs_dep = apport_et_besoin.besoin_ecs_depensier;
-  ecs.forEach((ecs) => calc_ecs(ecs, becs, becs_dep, GV, ca_id, zc_id, th));
+  ecs.forEach((ecs) => calc_ecs(ecs, becs, becs_dep, GV, ca_id, zc_id, th, virtualisationECS));
 
   const ac = cg.annee_construction;
   // needed for apport_et_besoin
@@ -136,7 +195,26 @@ export function calcul_3cl(dpe) {
   );
 
   const vt_list = logement.ventilation_collection.ventilation;
-  const conso = calc_conso(Sh, zc_id, ca_id, vt_list, instal_ch, ecs, clim);
+
+  let prorataECS = 1;
+
+  /**
+   * Besoins ECS pour les DPEs avec ECS collectif et répartition proratisés à la surface
+   *
+   * 15 - dpe issu d'une étude thermique réglementaire RT2012 bâtiment : appartement chauffage collectif ecs collectif
+   * 16 - dpe issu d'une étude thermique réglementaire RT2012 bâtiment : appartement chauffage individuel ecs collectif
+   * 19 - dpe issu d'une étude energie environement réglementaire RE2020 bâtiment : appartement chauffage collectif ecs collectif
+   * 20 - dpe issu d'une étude energie environement réglementaire RE2020 bâtiment : appartement chauffage individuel ecs collectif
+   * 22 - dpe issu d'une étude thermique réglementaire RT2012 bâtiment : appartement chauffage individuel ecs individuel
+   * 23 - dpe issu d'une étude thermique réglementaire RT2012 bâtiment : appartement chauffage collectif ecs individuel
+   * 24 - dpe issu d'une étude energie environement réglementaire RE2020 bâtiment : appartement chauffage collectif ecs individuel
+   * 25 - dpe issu d'une étude energie environement réglementaire RE2020 bâtiment : appartement chauffage individuel ecs individuel
+   */
+  if (['15', '16', '19', '20', '22', '23', '24', '25'].includes(map_id)) {
+    prorataECS = Sh / ShEcs;
+  }
+
+  const conso = calc_conso(Sh, zc_id, ca_id, vt_list, instal_ch, ecs, clim, prorataECS);
 
   const production_electricite = {
     conso_elec_ac: 0,
